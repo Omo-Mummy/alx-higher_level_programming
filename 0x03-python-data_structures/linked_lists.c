81dde939b9b6f52c8b1f148ea76b23949881a2e3
Omo-Mummy <olayinkaoyedele9@gmail.com>
2023-06-12 15:57:21 -0700

    tuples

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -9,19 +9,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *curr;
-    unsigned int a; /* number of nodes */
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-    curr = h;
-    a = 0;
-    while (curr != NULL)
+    current = h;
+    n = 0;
+    while (current != NULL)
     {
-	printf("%i\n", curr->n);
-	curr = curr->next;
-	a++;
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
     }
 
-    return (a);
+    return (n);
 }
 
 /**
@@ -33,24 +33,24 @@ size_t print_listint(const listint_t *h)
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
     listint_t *new;
-    listint_t *curr;
+    listint_t *current;
 
-    curr = *head;
+    current = *head;
 
     new = malloc(sizeof(listint_t));
     if (new == NULL)
-	return (NULL);
+        return (NULL);
 
     new->n = n;
     new->next = NULL;
 
     if (*head == NULL)
-	*head = new;
+        *head = new;
     else
     {
-	while (curr->next != NULL)
-	    curr = curr->next;
-	curr->next = new;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
     }
 
     return (new);
@@ -63,12 +63,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
  */
 void free_listint(listint_t *head)
 {
-    listint_t *curr;
+    listint_t *current;
 
     while (head != NULL)
     {
-	curr = head;
-	head = head->next;
-	free(curr);
+        current = head;
+        head = head->next;
+        free(current);
     }
 }
