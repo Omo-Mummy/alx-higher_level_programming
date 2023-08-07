55cc518a4607b2ea7bee15cca0afee1a9dbbcfe4
root <olayinkaoyedele9@gmail.com>
2023-08-07 15:50:23 -0700

    Hello-world

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str3 = "Holberton"
+str2 = "School"
+str3 += " " + str2
+print("Welcome to {}!".format(str3))
+
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_word_3 = word[:3]
+last_word_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(first_word_3))
+print("Last 2 letters: {}".format(last_word_2))
+print("Middle word: {}".format(middle_word))
+
